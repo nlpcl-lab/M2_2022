a86b3498739e8e0e306a381d63764d2ee507a845
huijelee <jae4258@naver.com>
2022-08-24 21:51:03 +0900

    implement main, evaluate

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -23,6 +23,7 @@ from augment_testor import AugmentorTester
 
 class AugmentorEvaluator(object):
     def __init__(self, hparams):
+        self.hparams = hparams
         self.test_docs = pd.read_pickle(hparams.test_fname)
         self.testors = self._load_testors(hparams.user2keywords)
         self.model = AutoModelForSeq2SeqLM.from_pretrained(
@@ -51,10 +52,45 @@ class AugmentorEvaluator(object):
     def evaluate_sents(self, original_text, augmented_text):
         mu1_o, sig1_o, mu1_a, sig1_a = self.testors[0].evaluate(original_text, augmented_text)
         mu2_o, sig2_o, mu2_a, sig2_a = self.testors[1].evaluate(original_text, augmented_text)
+        correct = (abs(mu1_o - mu2_o) > abs(mu1_a - mu2_a)) or (abs(sig1_o - sig2_o) > abs(sig1_a - sig2_a))
+
+        return np.array([correct])
+
+    def augment_sents(self, ctext):
+        batch_encoding = self.tokenizer(
+            ctext,
+            padding='max_length',
+            truncation=True,
+            max_length=self.hparams.max_seq_length,
+            return_tensors='np',
+        )
+        source_id, source_mask = batch_encoding['input_ids'], batch_encoding['attention_mask']
+        self.model.eval()
+        with torch.no_grad():
+            generated_ids = self.model.generate(
+                input_ids=source_id,
+                attention_mask=source_mask,
+                max_length=self.hparams.max_length,
+                truncation=True,
+                num_beams=3,
+                repetition_penalty=2.5,
+                length_penalty=1.0,
+                early_stopping=True
+            )
+            prediction = [self.tokenizer.batch_decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=True) for g in
+                          generated_ids]
+
+            return prediction
 
     def evaluate(self):
-        original_texts = self.docs.loc[0].values.tolist()
+        original_texts = self.test_docs.loc[0].values.tolist()
+        augmented_texts = self.augment_sents(original_texts)
+        total = np.array([])
+        for original_text, augmented_text in zip(original_texts, augmented_texts):
+            total = np.append(total, self.evaluate_sents(original_text, augmented_text))
 
+        percentage = total.sum() / total.shape[0]
+        print('증강률')
 
 def main(hparams):
     params = vars(hparams)
